442d1c02614d378a1d203eee5b49b22e09e7bd2e
whara123 <64121533+whara123@users.noreply.github.com>
2022-06-28 14:40:09 +0900

    ✨ utils 폴더에 애니메이션, 이미지, 지표데이터 생성

---
+++ src/utils/ImageStorage.js
@@ -0,0 +1,9 @@
+import playStore from '../image/play-store2x.png'
+import appleBadge from '../image/badge-apple4x.png'
+import tripleLogo from '../image/triple2x.png'
+
+export const ImageStroage = {
+  tripleLogo,
+  appleBadge,
+  playStore,
+}
---
+++ src/utils/IndicatorData.js
@@ -0,0 +1,14 @@
+export const indicatorData = [
+  {
+    content: '여행자',
+    targetNumber: 700,
+  },
+  {
+    content: '여행 리뷰',
+    targetNumber: 100,
+  },
+  {
+    content: '여행 일정',
+    targetNumber: 470,
+  },
+]
