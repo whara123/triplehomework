eb411a57d489d06f8726fb797b8b8d3e2194d5a3
whara123 <64121533+whara123@users.noreply.github.com>
2022-06-29 17:13:17 +0900

    💄 스타일 수정

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -4,17 +4,20 @@ export default createGlobalStyle`
  * {
     padding: 0;
     margin: 0;
+    box-sizing: border-box;
+
     font-size: inherit;
     font-family: sans-serif;
-    box-sizing: border-box;
   }
   html, body, #root {
     width: 100%;
     height: 100%;
+    
     background-color: #fff;
   }
   a {
     text-decoration : none;
+    
     color : inherit;
   }
   button {
